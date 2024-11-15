37f7cf15037caa63da4d962b3f98b8c2825e38c3
phinner <62483793+phinner@users.noreply.github.com>
2024-11-15 17:29:57 +0100

    chore(deps): Update KSR

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,1 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.xpdustry.com/releases") {
-            name = "xpdustry-releases"
-            mavenContent { releasesOnly() }
-        }
-        maven("https://maven.xpdustry.com/snapshots") {
-            name = "xpdustry-snapshots"
-            mavenContent { snapshotsOnly() }
-        }
-    }
-}
-
-// The project name, used as the name of the final artifact
 rootProject.name = "nohorny"
