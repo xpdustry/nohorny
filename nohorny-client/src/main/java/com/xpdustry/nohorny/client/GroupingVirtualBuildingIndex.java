5fb96e861f565331666e561da55990b352aa7e67
Phinner <62483793+phinner@users.noreply.github.com>
2026-04-22 23:52:12 +0200

    feat: Implemented an iterative grouping algorithm (#71)
    * feat: Implemented an iterative grouping algorithm, it allows much larger group chunks to be processed and confuses less the classification models as the image will remain complete.

--- nohorny-client/src/main/java/com/xpdustry/nohorny/client/GroupingVirtualBuildingIndex.java
+++ nohorny-client/src/main/java/com/xpdustry/nohorny/client/GroupingVirtualBuildingIndex.java
@@ -7,8 +7,7 @@ import arc.struct.IntSet;
 import com.xpdustry.nohorny.common.GeometryUtils;
 import com.xpdustry.nohorny.common.VirtualBuilding;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Objects;
+import java.util.List;
 import org.jspecify.annotations.Nullable;
 
 final class GroupingVirtualBuildingIndex<T> extends VirtualBuildingIndex<T> {
@@ -68,67 +67,110 @@ final class GroupingVirtualBuildingIndex<T> extends VirtualBuildingIndex<T> {
         return building;
     }
 
-    public VirtualBuilding.@Nullable Group<T> groupAt(
-            final int initialX, final int initialY, final int range, final IntSet visited) {
-        final var building = this.select(initialX, initialY);
-        if (building == null || !visited.add(GeometryUtils.pack(building.x(), building.y()))) {
-            return null;
-        }
-
-        final var queue = new IntQueue();
-        final var halfRange = Math.floorDiv(range, 2);
-        int minX = building.x();
-        int minY = building.y();
-        int maxX = building.x() + building.size();
-        int maxY = building.y() + building.size();
-
-        final var buildings = new ArrayList<VirtualBuilding<T>>();
-        queue.addLast(GeometryUtils.pack(building.x(), building.y()));
-
-        while (!queue.isEmpty()) {
-            final var visitingPacked = queue.removeFirst();
-            final var visiting = Objects.requireNonNull(this.index.get(visitingPacked));
-
-            minX = Math.min(minX, visiting.x());
-            minY = Math.min(minY, visiting.y());
-            maxX = Math.max(maxX, visiting.x() + visiting.size());
-            maxY = Math.max(maxY, visiting.y() + visiting.size());
-            buildings.add(visiting);
-
-            final var set = this.adjacencyUnion.get(visitingPacked);
-            if (set == null) {
-                continue;
-            }
-            final var iterator = set.iterator();
-            while (iterator.hasNext) {
-                final var neighborPacked = iterator.next();
-                final var neighbor = this.index.get(neighborPacked);
-                // That's not supposed to happen, but we never know...
-                if (neighbor == null) {
-                    iterator.remove();
-                    continue;
-                }
-                if (!overlaps(
-                        initialX - halfRange,
-                        initialY - halfRange,
-                        range,
-                        neighbor.x(),
-                        neighbor.y(),
-                        neighbor.size())) {
-                    continue;
-                }
-                if (visited.add(neighborPacked)) {
-                    queue.addLast(neighborPacked);
-                }
-            }
-        }
-
-        return new VirtualBuilding.Group<>(
-                minX, minY, maxX - minX, maxY - minY, Collections.unmodifiableList(buildings));
+    public Grouper startGrouperAt(final int initialX, final int initialY, final int range, final int steps) {
+        return new Grouper(initialX, initialY, range, steps);
     }
 
     private static boolean overlaps(
             final int x1, final int y1, final int size1, final int x2, final int y2, final int size2) {
         return (x1 < x2 + size2 && x2 < x1 + size1) && (y1 < y2 + size2 && y2 < y1 + size1);
+    }
+
+    // NOTE: We are not dealing with deleted buildings, this solution is good enough...
+    public final class Grouper {
+
+        private final int initialX;
+        private final int initialY;
+        private final int range;
+        private final int maxSteps;
+        private final int halfRange;
+        private final IntQueue queue = new IntQueue();
+        private final IntSet visited = new IntSet();
+        private final List<VirtualBuilding<T>> buildings = new ArrayList<>();
+
+        private int minX;
+        private int minY;
+        private int maxX;
+        private int maxY;
+
+        private Grouper(final int initialX, final int initialY, final int range, final int maxSteps) {
+            this.initialX = initialX;
+            this.initialY = initialY;
+            this.range = range;
+            this.halfRange = Math.floorDiv(range, 2);
+            this.maxSteps = maxSteps;
+
+            final var building = GroupingVirtualBuildingIndex.this.select(initialX, initialY);
+            if (building == null) {
+                return;
+            }
+
+            this.visited.add(GeometryUtils.pack(building.x(), building.y()));
+            this.queue.addLast(GeometryUtils.pack(building.x(), building.y()));
+
+            this.minX = building.x();
+            this.minY = building.y();
+            this.maxX = building.x() + building.size();
+            this.maxY = building.y() + building.size();
+        }
+
+        public void progress() {
+            for (int i = 0; i < this.maxSteps && !this.isCompleted(); i++) {
+                final var visitingPacked = this.queue.removeFirst();
+                final var visiting = GroupingVirtualBuildingIndex.this.index.get(visitingPacked);
+                if (visiting == null) {
+                    continue;
+                }
+
+                this.minX = Math.min(this.minX, visiting.x());
+                this.minY = Math.min(this.minY, visiting.y());
+                this.maxX = Math.max(this.maxX, visiting.x() + visiting.size());
+                this.maxY = Math.max(this.maxY, visiting.y() + visiting.size());
+                this.buildings.add(visiting);
+
+                final var set = GroupingVirtualBuildingIndex.this.adjacencyUnion.get(visitingPacked);
+                if (set == null) {
+                    continue;
+                }
+                final var iterator = set.iterator();
+                while (iterator.hasNext) {
+                    final var neighborPacked = iterator.next();
+                    final var neighbor = GroupingVirtualBuildingIndex.this.index.get(neighborPacked);
+                    // That's not supposed to happen, but we never know...
+                    if (neighbor == null) {
+                        iterator.remove();
+                        continue;
+                    }
+                    if (!overlaps(
+                            this.initialX - this.halfRange,
+                            this.initialY - this.halfRange,
+                            this.range,
+                            neighbor.x(),
+                            neighbor.y(),
+                            neighbor.size())) {
+                        continue;
+                    }
+                    if (this.visited.add(neighborPacked)) {
+                        this.queue.addLast(neighborPacked);
+                    }
+                }
+            }
+        }
+
+        public boolean isCompleted() {
+            return this.queue.isEmpty();
+        }
+
+        public boolean isVisited(final int packed) {
+            return this.visited.contains(packed);
+        }
+
+        public VirtualBuilding.@Nullable Group<T> create() {
+            if (this.buildings.isEmpty()) {
+                return null;
+            }
+            return new VirtualBuilding.Group<>(
+                    this.minX, this.minY, this.maxX - this.minX, this.maxY - this.minY, List.copyOf(this.buildings));
+        }
     }
 }
