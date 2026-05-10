b4a90eaed92c4eec99a8a0f2ffcb2398a03a364a
Finley Experience <62483793+phinner@users.noreply.github.com>
2026-05-10 18:57:07 +0200

    fix: Cap size of rendered images to avoid DOS and OOMs

--- nohorny-common/src/main/java/com/xpdustry/nohorny/common/MindustryImageRenderer.java
+++ nohorny-common/src/main/java/com/xpdustry/nohorny/common/MindustryImageRenderer.java
@@ -7,30 +7,38 @@ import java.awt.image.BufferedImage;
 
 public final class MindustryImageRenderer {
 
+    private static final int MAX_IMAGE_SIZE = 2048;
     private static final int BLOCK_SIZE = 32;
 
     private MindustryImageRenderer() {}
 
     public static BufferedImage render(final VirtualBuilding.Group<? extends MindustryImage> group) {
-        final var image =
-                new BufferedImage(group.w() * BLOCK_SIZE, group.h() * BLOCK_SIZE, BufferedImage.TYPE_INT_ARGB);
+        final long renderW = NoHornyPreconditions.positive((long) group.w() * BLOCK_SIZE, "group w");
+        final long renderH = NoHornyPreconditions.positive((long) group.h() * BLOCK_SIZE, "group h");
+
+        final double downscaling =
+                Math.min(1D, Math.min(MAX_IMAGE_SIZE / (double) renderW, MAX_IMAGE_SIZE / (double) renderH));
+        final int imageW = Math.min(MAX_IMAGE_SIZE, (int) Math.ceil(renderW * downscaling));
+        final int imageH = Math.min(MAX_IMAGE_SIZE, (int) Math.ceil(renderH * downscaling));
+        final var image = new BufferedImage(imageW, imageH, BufferedImage.TYPE_INT_ARGB);
+
         try (final var scope = new GraphicsScope(image)) {
             scope.graphics().setColor(Color.BLACK);
             scope.graphics().fillRect(0, 0, image.getWidth(), image.getHeight());
-
             // Invert y-axis, because Mindustry uses bottom-left as the origin
-            scope.graphics().translate(0, image.getHeight());
-            scope.graphics().scale(1, -1);
+            // The flip also includes downscaling in case the virtual image is too big
+            scope.graphics().translate(0, imageH);
+            scope.graphics().scale(downscaling, -downscaling);
 
             for (final var building : group.elements()) {
                 final int x = (building.x() - group.x()) * BLOCK_SIZE;
                 final int y = (building.y() - group.y()) * BLOCK_SIZE;
                 final int size = building.size() * BLOCK_SIZE;
-                try (final var sub =
+                try (final var childScope =
                         new GraphicsScope((Graphics2D) scope.graphics().create(x, y, size, size))) {
-                    final var scale = (double) size / building.data().resolution();
-                    sub.graphics().scale(scale, scale);
-                    MindustryImageRenderer.renderWith(sub.graphics(), building.data());
+                    final var childScale = (double) size / building.data().resolution();
+                    childScope.graphics().scale(childScale, childScale);
+                    MindustryImageRenderer.renderWith(childScope.graphics(), building.data());
                 }
             }
         }
