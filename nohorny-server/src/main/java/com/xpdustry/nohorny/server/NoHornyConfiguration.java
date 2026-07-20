38262db26147c2642627c9e8c3a316c10097be4d
phinner <62483793+Phinner@users.noreply.github.com>
2026-07-20 20:35:23 +0200

    fix exception due to github returning text mime type instead of json

--- nohorny-server/src/main/java/com/xpdustry/nohorny/server/NoHornyConfiguration.java
+++ nohorny-server/src/main/java/com/xpdustry/nohorny/server/NoHornyConfiguration.java
@@ -3,8 +3,10 @@ package com.xpdustry.nohorny.server;
 
 import java.net.http.HttpClient;
 import java.time.Duration;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
@@ -28,9 +30,12 @@ public class NoHornyConfiguration {
                 .followRedirects(HttpClient.Redirect.ALWAYS)
                 .build());
         requestFactory.setReadTimeout(Duration.ofSeconds(30));
+        final var jsonConverter = new JacksonJsonHttpMessageConverter(mapper);
+        // GitHub raw endpoints return text plain for json files, how annoying
+        jsonConverter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN));
         return RestClient.builder()
                 .configureMessageConverters(converters -> converters
-                        .addCustomConverter(new JacksonJsonHttpMessageConverter(mapper))
+                        .addCustomConverter(jsonConverter)
                         .addCustomConverter(new ResourceHttpMessageConverter()))
                 .requestFactory(requestFactory)
                 .build();
