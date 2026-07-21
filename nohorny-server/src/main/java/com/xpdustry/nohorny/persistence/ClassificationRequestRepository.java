d42df43a651d5addbab2b2364bfb50d8e82e8f08
phinner <62483793+phinner@users.noreply.github.com>
2026-07-21 11:35:25 +0200

    Make old request cleanup transactional

--- nohorny-server/src/main/java/com/xpdustry/nohorny/persistence/ClassificationRequestRepository.java
+++ nohorny-server/src/main/java/com/xpdustry/nohorny/persistence/ClassificationRequestRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ClassificationRequestRepository extends JpaRepository<ClassificationRequest, Long> {
 
@@ -16,6 +17,7 @@ public interface ClassificationRequestRepository extends JpaRepository<Classific
     Optional<ClassificationRequestImage> findImageById(final long id);
 
     @Modifying
+    @Transactional
     @Query(nativeQuery = true, value = """
             DELETE FROM classification_request
             WHERE id NOT IN (SELECT id FROM classification_request ORDER BY id DESC LIMIT :capacity)""")
