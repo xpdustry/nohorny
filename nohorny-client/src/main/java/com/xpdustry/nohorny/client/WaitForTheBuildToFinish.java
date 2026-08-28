75970aad323c9001a12dd07cb58044d4ac5e8243
phinner <62483793+Phinner@users.noreply.github.com>
2026-08-28 16:15:46 +0200

    Double the WaitForTheBuildToFinish counter

--- nohorny-client/src/main/java/com/xpdustry/nohorny/client/WaitForTheBuildToFinish.java
+++ nohorny-client/src/main/java/com/xpdustry/nohorny/client/WaitForTheBuildToFinish.java
@@ -37,6 +37,7 @@ final class WaitForTheBuildToFinish {
             }
             this.counter = Math.max(this.counter, ticks);
         }
+        this.counter *= 2;
     }
 
     public void countdown() {
